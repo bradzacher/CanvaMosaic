521c99b1981a979f7d7429842bfb3f3770dcd636
Brad Zacher <brad.zacher@gmail.com>
2016-11-06 20:24:09 +1030

    moved tile calculation to background threads
    makes it run a bit nicer

---
+++ js/generator/tileCalculationWorker.js
@@ -0,0 +1,126 @@
+/**
+ * Input Messages:
+ *  {
+ *      image: {        // the size of the source image
+ *          width, height
+ *      },
+ *      tiles: [        // the list of tiles to process
+ *          {
+ *              origin: {   // the top-left corner of the tile
+ *                  x, y,
+ *              },
+ *              id,         // a unique identifier for the tile
+ *              sourceTile  // the ImageData object
+ *          }
+ *      ]
+ *  }
+ *
+ * Output Messages:
+ *  {
+ *      tiles: [ // list of results
+ *          {
+ *              average: {
+ *                  raw: { // the raw average
+ *                      r, g, b
+ *                  },
+ *                  hex // the average represented as a hex string
+ *              },
+ *              id // the unique identifier for the tile the average was calculated from
+ *          }
+ *      ]
+ *  }
+ */
+
+
+// converts an integer to a two digit hex number
+function toTwoDigitHex(integer) {
+    // round and conver to hex
+    const hex = Math.round(integer).toString(16);
+    // make sure 2 digits
+    return `0${hex}`.slice(-2);
+}
+
+self.addEventListener('message', (e) => {
+    const tiles = e.data.tiles;
+    if (!tiles) {
+        throw new Error('Expected message to contain a "tiles" property.');
+    }
+    const image = e.data.image;
+    if (!tiles) {
+        throw new Error('Expected message to contain a "image" property.');
+    }
+
+    const returnTiles = [];
+
+    tiles.forEach((t) => {
+        const average = {
+            r: 0,
+            g: 0,
+            b: 0,
+            // a: 0,
+        };
+        let pixelCount = 0;
+
+        const sourceTile = t.sourceTile;
+        const pixels = sourceTile.data;
+        const byteWidth = sourceTile.width * 4;
+
+        // If the image is bigger than a multiple of TILE_WIDTH x TILE_HEIGHT
+        // then the last tile in the row/column will be heavily weighted toward black
+        // because of how the image data object works (it marks the pixels as black!).
+
+        // check if the tile contains out of bound pixels
+        // we do this so we can short circuit the if checks inside the loop
+        const containsPixelsOutsideWidthBound = t.x + sourceTile.width > image.width;
+        const containsPixelsOutsideHeightBound = t.y + sourceTile.height > image.height;
+
+        // sum the colour values for the tile's pixels
+        for (let y = 0; y < sourceTile.height; y += 1) {
+            if (containsPixelsOutsideHeightBound &&
+                y + t.y >= image.height) {
+                // outside bounds - ignore
+                continue;
+            }
+            for (let x = 0; x < sourceTile.width; x += 1) {
+                if (containsPixelsOutsideWidthBound &&
+                    x + t.x >= image.width) {
+                    // outside bounds - ignore
+                    continue;
+                }
+
+                // calculate the flat index
+                const i = (y * byteWidth) + (x * 4);
+
+                average.r += pixels[i + 0];
+                average.g += pixels[i + 1];
+                average.b += pixels[i + 2];
+                // average.a += pixels[i + 3];
+
+                pixelCount += 1;
+            }
+        }
+
+        // calculate the averages
+        average.r /= pixelCount;
+        average.g /= pixelCount;
+        average.b /= pixelCount;
+        // average.a /= pixelCount;
+
+        // convert to hex
+        const hexAvg = toTwoDigitHex(average.r) + toTwoDigitHex(average.g) + toTwoDigitHex(average.b);
+
+        // prepare it to be returned
+        returnTiles.push({
+            average: {
+                raw: average,
+                hex: hexAvg,
+            },
+            id: t.id,
+        });
+    });
+
+    // send the results back
+    postMessage({
+        tiles: returnTiles,
+    });
+});
--- js/generator/tileGenerator.js
+++ js/generator/tileGenerator.js
@@ -6,12 +6,10 @@ const inputImage = document.getElementById('inputImage');
 const tileResult = document.getElementById('tileResult');
 const svgResult  = document.getElementById('result');
 
-// converts an integer to a two digit hex number
-function toTwoDigitHex(integer) {
-    // round and conver to hex
-    const hex = Math.round(integer).toString(16);
-    // make sure 2 digits
-    return `0${hex}`.slice(-2);
+// splits an array into chunks of size n
+// adapted from http://stackoverflow.com/a/10456644/3736051
+function chunkArray(arr, n) {
+    return Array.from(Array(Math.ceil(arr.length / n)), (x, i) => arr.slice(i * n, (i * n) + n));
 }
 
 function generateTiles(dataUrl) {
@@ -49,126 +47,122 @@ function generateTiles(dataUrl) {
                 sourceTile,
                 destTile,
                 destCtx: tileResultCtx,
+                id: tiles.length,
             });
         }
     }
 
-    // calculate the averages for each tile
-    tiles.forEach((t) => {
-        const average = {
-            r: 0,
-            g: 0,
-            b: 0,
-            // a: 0,
-        };
-        let pixelCount = 0;
-
-        const destTile = t.destTile;
-        const destPixels = destTile.data;
-
-        const sourceTile = t.sourceTile;
-        const pixels = sourceTile.data;
-        const byteWidth = sourceTile.width * 4;
-
-        // If the image is bigger than a multiple of TILE_WIDTH x TILE_HEIGHT
-        // then the last tile in the row/column will be heavily weighted toward black
-        // because of how the image data object works (it marks the pixels as black!).
-
-        // check if the tile contains out of bound pixels
-        // we do this so we can short circuit the if checks inside the loop
-        const containsPixelsOutsideWidthBound = t.x + sourceTile.width > image.width;
-        const containsPixelsOutsideHeightBound = t.y + sourceTile.height > image.height;
-
-        // sum the colour values for the tile's pixels
-        for (let y = 0; y < sourceTile.height; y += 1) {
-            if (containsPixelsOutsideHeightBound &&
-                y + t.y >= image.height) {
-                // outside bounds - ignore
-                continue;
-            }
-            for (let x = 0; x < sourceTile.width; x += 1) {
-                if (containsPixelsOutsideWidthBound &&
-                    x + t.x >= image.width) {
-                    // outside bounds - ignore
-                    continue;
-                }
-
-                // calculate the flat index
-                const i = (y * byteWidth) + (x * 4);
-
-                average.r += pixels[i + 0];
-                average.g += pixels[i + 1];
-                average.b += pixels[i + 2];
-                // average.a += pixels[i + 3];
-
-                pixelCount += 1;
-            }
-        }
-
-        // calculate the averages
-        average.r /= pixelCount;
-        average.g /= pixelCount;
-        average.b /= pixelCount;
-        // average.a /= pixelCount;
-
-        // convert to hex
-        t.average = toTwoDigitHex(average.r) + toTwoDigitHex(average.g) + toTwoDigitHex(average.b);
-
-        // draw the average colour to the intermediate canvas
-        for (let i = 0; i < destPixels.length; i += 4) {
-            destPixels[i + 0] = average.r;
-            destPixels[i + 1] = average.g;
-            destPixels[i + 2] = average.b;
-            destPixels[i + 3] = 255;
-            // destPixels[i + 3] = average.a;
-        }
-        t.destCtx.putImageData(destTile, t.x, t.y);
-    });
-
-    // use promises to queue the drawing of the SVGs
-    // each row can't render until the previous one renders
-
-    // tracks the list of promises for the current row
-    let rowPromises = [];
-    // tracks the current row's div container
-    let currentRowDiv = document.createElement('div');
-    // tracks the previous column's completion promise (used to prevent columns rendering out of order)
-    let columnPromise = Promise.resolve();
-
-    // ties off the last row and sets up its render process
-    function finaliseRow() {
-        const lastRowDiv = currentRowDiv;
-        // set the last row to renderr
-        const lastRowRenderPromise = Promise.all(rowPromises).then(() => {
-            svgResult.appendChild(lastRowDiv);
+    const workerPromises = [];
+    // split the set of tiles into packages of work for background threads
+    const workPackages = chunkArray(tiles, 100);
+    // calculate the averages for each tile by sending them to the workers
+    workPackages.forEach((tileChunk) => {
+        // build the payload to send to the worker
+        const tilePayload = [];
+        tileChunk.forEach((t) => {
+            tilePayload.push({
+                origin: {
+                    x: t.x,
+                    y: t.y,
+                },
+                id: t.id,
+                sourceTile: t.sourceTile,
+            });
         });
+        const message = {
+            image: {
+                width: image.width,
+                height: image.height,
+            },
+            tiles: tilePayload,
+        };
 
-        // start the new row with the promise from the last (so the new row can't render until the last one does)
-        rowPromises = [lastRowRenderPromise];
-        currentRowDiv = document.createElement('div');
-    }
+        // build the worker
+        const worker = new Worker('js/generator/tileCalculationWorker.js');
 
-    tiles.forEach((t, i) => {
-        // are we starting a new row?
-        if (i % tileColumns === 0 && i !== 0) {
-            finaliseRow();
+        const prom = new Promise((resolve) => {
+            // listen for responses
+            worker.addEventListener('message', (msg) => {
+                const tileAverages = msg.data.tiles;
+
+                // save the averages back to the original tile objects
+                tileAverages.forEach((t) => {
+                    const tile = tiles[t.id];
+                    tile.average = t.average.hex;
+
+                    // draw the average colour to the intermediate canvas
+                    const average = t.average.raw;
+                    const destPixels = tile.destTile.data;
+                    for (let i = 0; i < destPixels.length; i += 4) {
+                        destPixels[i + 0] = average.r;
+                        destPixels[i + 1] = average.g;
+                        destPixels[i + 2] = average.b;
+                        destPixels[i + 3] = 255;
+                        // destPixels[i + 3] = average.a;
+                    }
+                    tile.destCtx.putImageData(tile.destTile, tile.x, tile.y);
+                });
+
+                // we don't need the worker any more, so free up the resources
+                worker.terminate();
+
+                resolve();
+            });
+
+            // send the message
+            worker.postMessage(message);
+        });
+        // save the promise so we can await all of the completions
+        workerPromises.push(prom);
+    });
+
+    // once all of the tiles have been calculated, we can start fetching them all
+    Promise.all(workerPromises).then(() => {
+        // use promises to queue the drawing of the SVGs
+        // each row can't render until the previous one renders
+
+        // tracks the list of promises for the current row
+        let rowPromises = [];
+        // tracks the current row's div container
+        let currentRowDiv = document.createElement('div');
+        // tracks the previous column's completion promise (used to prevent columns rendering out of order)
+        let columnPromise = Promise.resolve();
+
+        // ties off the last row and sets up its render process
+        function finaliseRow() {
+            const lastRowDiv = currentRowDiv;
+            // set the last row to renderr
+            const lastRowRenderPromise = Promise.all(rowPromises).then(() => {
+                svgResult.appendChild(lastRowDiv);
+            });
+
+            // start the new row with the promise from the last (so the new row can't render until the last one does)
+            rowPromises = [lastRowRenderPromise];
+            currentRowDiv = document.createElement('div');
         }
 
-        // locally reference the div so we keep it in our closure
-        const currentRow = currentRowDiv;
-        const lastColPromise = columnPromise;
+        tiles.forEach((t, i) => {
+            // are we starting a new row?
+            if (i % tileColumns === 0 && i !== 0) {
+                finaliseRow();
+            }
 
-        const fetchPromise = fetchQueue(`color/${t.average}`)
-            .then(svg => lastColPromise.then(() => {
-                // add the svg to the row - but only after the previous row has finished rendering
-                currentRow.innerHTML += svg;
-            }));
+            // locally reference the div so we keep it in our closure
+            const currentRow = currentRowDiv;
+            const lastColPromise = columnPromise;
 
-        rowPromises.push(fetchPromise);
-        columnPromise = fetchPromise;
+            const fetchPromise = fetchQueue(`color/${t.average}`)
+                .then(svg => lastColPromise.then(() => {
+                    // add the svg to the row - but only after the previous row has finished rendering
+                    currentRow.innerHTML += svg;
+                }));
+
+            rowPromises.push(fetchPromise);
+            columnPromise = fetchPromise;
+        });
+        // finalise the last row of the mosaic
+        finaliseRow();
     });
-    // finalise the last row of the mosaic
-    finaliseRow();
 }
 
 module.exports = generateTiles;
